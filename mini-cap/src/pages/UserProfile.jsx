2d8dbd3ce15316d3ae0988fca82e985d241cb935
laurenrigante <lrigante@hotmail.com>
2024-01-31 20:53:49 -0500

    updated user profile and css

--- mini-cap/src/pages/UserProfile.jsx
+++ mini-cap/src/pages/UserProfile.jsx
@@ -1,9 +1,168 @@
 import "../styling/profile.css"
+import { toast } from "react-toastify";
+import React, { useState, useEffect } from "react";
+import DeleteModal from "../components/DeleteModal";
+import { Link, useNavigate } from "react-router-dom";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
 
 const UserProfile =() => {
 
+  const navigate = useNavigate();
+
+  const [isEditMode, setIsEditMode] = useState(false);
+  const [previewUrl, setPreviewUrl] = useState(null);
+  const [file, setFile] = useState(null);
+  const [profilePicUrl, setProfilePicUrl] = useState(null);
+  const [show, setShow] = useState(false);
+
+  const [firstName, setFirstName] = useState(null);
+  const [lastName, setLastName] = useState(null);
+  const [email, setEmail] = useState(null);
+  const [phoneNumber, setPhoneNumber] = useState(null);
+  const [currentPassword, setCurrentPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [role, setRole] = useState(null);
+
+
+  
+  //edit button
+  const handleEditClick = () => {
+    setIsEditMode(true);
+  };
+
+  //save button
+  const handleSaveClick = async (ev) => {
+    ev.preventDefault();
+    if (!firstName || !lastName)
+      return toast.error("Please make sure all required fields aren't empty");
+
+
+    if (phoneNumber && !/^\d{10}$/.test(phoneNumber))
+      return toast.error("Please make sure the phone number format is correct");
+    const formData = {
+      firstName,
+      lastName,
+      phoneNumber,
+    };
+    //await updateUserInfo(formData);
+    toast.success("User info updated successfully");
+    setIsEditMode(false);
+  };
+
+
+    //photo change
+    const handlePhotoChange = (event) => {
+      const photo = event.target.files[0];
+  
+      if (
+        photo.type !== "image/png" &&
+        photo.type !== "image/jpeg" &&
+        photo.type !== "image/jpg"
+      ) {
+        return toast.error("File not supported");
+      }
+      if (photo.size > 2097152) return toast.error("File must be less than 2 MB");
+  
+      setProfilePicUrl(photo);
+      const fileReader = new FileReader();
+      fileReader.onload = () => {
+        setPreviewUrl(fileReader.result);
+      };
+      fileReader.readAsDataURL(photo);
+    };
+
+
+    //submitting photo to backend
+    const handleSubmitPhoto = async (event) => {
+      event.preventDefault();
+  
+      // create a new FormData object to send the file
+      const formData = new FormData();
+      formData.append("image", profilePicUrl);
+
+      // make a POST request to the backend to upload the image
+     
+    };
+
+    
+  const handleClickDelete = (id) => {
+    setShow(true);
+  };
+
+  
+  //delete user
+  const handleClose = () => {
+    setShow(false);
+  };
+
+  
+  //delete function
+  const deleteAccount = () => {
+   // dispatch(deleteUser);
+    toast.success("Account deleted successfully");
+    navigate("/"); //link to registration page instead 
+    setShow(false);
+  };
+  
+  //cancel button
+  const handleCancelClick = () => {
+    setIsEditMode(false);
+    window.location.reload();
+  };
+
+  //changes
+  const handleFirstNameChange = (ev) => {
+    setFirstName(ev.target.value);
+  };
+  const handleLastNameChange = (ev) => {
+    setLastName(ev.target.value);
+  };
+  const handlePhoneNumberChange = (ev) => {
+    setPhoneNumber(ev.target.value);
+  };
+  const handleCurrentPasswordChange = (ev) => {
+    setCurrentPassword(ev.target.value);
+  };
+  const handleNewPasswordChange = (ev) => {
+    setNewPassword(ev.target.value);
+  };
+  const handleConfirmPasswordChange = (ev) => {
+    setConfirmPassword(ev.target.value);
+  };
+  const handleChangePassword = async (ev) => {
+    ev.preventDefault();
+
+    if (!currentPassword || !newPassword || !confirmPassword)
+      return toast.error("Please make sure all password fields are filled out");
+
+    if (newPassword !== confirmPassword)
+      return toast.error("New password do not match");
+
+    if (newPassword.length < 8)
+      return toast.error("Password must be at least 8 characters");
+/* 
+   // setIsLoading(true);
+    const dataForm = {
+      currentPassword,
+      newPassword,
+    };
+
+    const data = await changePassword(dataForm);
+    //setIsLoading(false);
+
+    if (data?.message === "Password updated successfully") {
+      setCurrentPassword("");
+      setNewPassword("");
+      setConfirmPassword("");
+      toast.success(data.message);
+    } */
+  };
+
     return(
        <div>
+        <Header/>
         <div style={{ backgroundColor: "#f8f9fa" }}>
       <link
         rel="stylesheet"
@@ -19,16 +178,31 @@ const UserProfile =() => {
                 <div className="card">
                   <div className="card-body">
                     <div className="d-flex flex-column align-items-center text-center">
+                    {previewUrl ? (
+                        <img
+                          src={previewUrl}
+                          alt="profile.jpg"
+                          className="rounded-circle"
+                          style={{
+                            width: "150px",
+                            height: "150px",
+                            objectFit: "cover",
+                          }}
+                        />
+                      ) : (
+                        <img
+                          src={profilePicUrl}
+                          alt="profile.jpg"
+                          className="rounded-circle"
+                          width={150}
+                        />
+                      )}
 
-                      <div className="mt-3">
-                        <p className="text-secondary mb-1">
-                        </p>
-                      </div>
                     </div>
                   </div>
                 </div>
 
-                <form>
+                <form onSubmit={handleSubmitPhoto}>
                   <label className="form-label mt-3" htmlFor="customFile">
                     Choose an image:
                   </label>
@@ -39,6 +213,7 @@ const UserProfile =() => {
                         type="file"
                         className="form-control"
                         id="customFile"
+                        onChange={handlePhotoChange}
                       />
                     </div>
                     <div className="col-sm-4">
@@ -53,9 +228,19 @@ const UserProfile =() => {
                 <button
                   type="button"
                   className="btn btn-danger btn-block mt-2"
+                  onClick={() => handleClickDelete()}
                 >
                   Delete Account
                 </button>
+                <DeleteModal
+                  show={show}
+                  handleClose={handleClose}
+                  handleDeleteItem={deleteAccount}
+                  message={
+                    "All information will be deleted, are you sure you want to proceed?"
+                  }
+                />
+
               </div>
               <div className="col-md-8">
                 <div className="card mb-3">
@@ -65,7 +250,23 @@ const UserProfile =() => {
                         <h6 className="mb-0">First Name</h6>
                       </div>
                       <div className="col-sm-9 text-secondary">
-                      
+                      {isEditMode ? (
+                          <input
+                            type="text"
+                            className={
+                              "form-control" +
+                              (firstName === "" ? " is-invalid" : "")
+                            }
+                            name="firstName"
+                            value={firstName}
+                            onChange={handleFirstNameChange}
+                          />
+                        ) : (
+                          <span>{firstName}</span>
+                        )}
+
+
+
                       </div>
                     </div>
                     <hr />
@@ -74,7 +275,20 @@ const UserProfile =() => {
                         <h6 className="mb-0">Last Name</h6>
                       </div>
                       <div className="col-sm-9 text-secondary">
-                   
+                      {isEditMode ? (
+                          <input
+                            type="text"
+                            className={
+                              "form-control" +
+                              (lastName === "" ? " is-invalid" : "")
+                            }
+                            name="lastName"
+                            value={lastName}
+                            onChange={handleLastNameChange}
+                          />
+                        ) : (
+                          <span>{lastName}</span>
+                        )}
                       </div>
                     </div>
                     <hr />
@@ -84,6 +298,17 @@ const UserProfile =() => {
                         <h6 className="mb-0">Email</h6>
                       </div>
                       <div className="col-sm-9 text-secondary">
+                      {isEditMode ? (
+                          <input
+                            type="text"
+                            className={"form-control"}
+                            name="email"
+                            value={email}
+                            disabled
+                          />
+                        ) : (
+                          <span>{email}</span>
+                        )}
                       </div>
                     </div>
                     <hr />
@@ -92,7 +317,21 @@ const UserProfile =() => {
                         <h6 className="mb-0">Phone</h6>
                       </div>
                       <div className="col-sm-9 text-secondary">
-                       
+                      {isEditMode ? (
+                          <input
+                            type="number"
+                            min="0"
+                            className="form-control"
+                            name="phoneNumber"
+                            value={phoneNumber || ""}
+                            placeholder="Phone Number"
+                            onChange={handlePhoneNumberChange}
+                          />
+                        ) : (
+                          <span>
+                            {phoneNumber ? phoneNumber : <>+1(XXX) XXX-XXXX</>}
+                          </span>
+                        )}
                       </div>
                     </div>
                     <hr />
@@ -101,25 +340,57 @@ const UserProfile =() => {
                         <h6 className="mb-0">User type</h6>
                       </div>
                       <div className="col-sm-9 text-secondary text-capitalize">
-                       
+                      {isEditMode ? (
+                          <input
+                            type="text"
+                            className={"form-control text-capitalize"}
+                            name="role"
+                            value={role}
+                            disabled
+                          />
+                        ) : (
+                          <span>{role}</span>
+                        )}
                       </div>
                     </div>
                     <hr />
+
+                    <div className="row">
+                      <div className="col-sm-12 d-flex justify-content-between">
+                        {isEditMode ? (
+                          <>
+                            <button
+                              className="btn saveChanges"
+                              onClick={handleSaveClick}
+                            >
+                              Save Changes
+                            </button>
+                            <button
+                              className="btn btn-secondary"
+                              style={{
+                                width: "250px",
+                              }}
+                              onClick={() => handleCancelClick()}
+                            >
+                              Cancel Changes
+                            </button>
+                          </>
+                        ) : (
                     <button
                             className="btn editProfile"
+                            onClick={() => handleEditClick()}
                           >
                             Edit Profile
                           </button>
-                    
-                    <div className="row">
-                      <div className="col-sm-12 d-flex justify-content-between">
-                   
+                       )}
+                
                       </div>
                     </div>
                   </div>
                 </div>
                 <form
                   className="row gutters-md"
+                  onSubmit={handleChangePassword}
                 >
                   <div className="col-md-12">
                     <div className="card mb-3">
@@ -135,7 +406,9 @@ const UserProfile =() => {
                               type="password"
                               className="form-control"
                               name="currentPassword"
-                              placeholder="**********"                         
+                              placeholder="**********"  
+                              value={currentPassword}
+                              onChange={handleCurrentPasswordChange}                       
                             />
                           </div>
                         </div>
@@ -151,6 +424,8 @@ const UserProfile =() => {
                               className="form-control"
                               name="newPassword"
                               placeholder="**********"
+                              value={newPassword}
+                              onChange={handleNewPasswordChange}
                             />
                           </div>
                         </div>
@@ -166,6 +441,8 @@ const UserProfile =() => {
                               className="form-control"
                               name="confirmPassword"
                               placeholder="**********"
+                              value={confirmPassword}
+                              onChange={handleConfirmPasswordChange}
                             />
                           </div>
                         </div>
@@ -182,6 +459,7 @@ const UserProfile =() => {
         </div>
       </div>
     </div>
+    <Footer/>
        </div> 
     );
 };
