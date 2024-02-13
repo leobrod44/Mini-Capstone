68f804fb8cb7c1d10309de9b6039cc33667583aa
lauren rigante <93160873+laurenrigante@users.noreply.github.com>
2024-02-13 14:48:33 -0500

    add key popup for condo owner /renter

---
+++ mini-cap/src/components/Popup.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "../styling/Popup.css";
+import "../index.css";
+
+const Popup = ({ handleClose }) => {
+  return (
+    <div className="popup_container">
+      <div className="popup_content">
+        <span className="close_popup" onClick={handleClose}>
+          &times;
+        </span>
+        <h4 className="h4_db">Register your condo</h4>
+        <form onSubmit={handleClose}>
+          <label className="key_label" htmlFor="key">Key:</label>
+          <input type="text" id="key" />
+          <button className="btn-reg" type="submit">Submit Key</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Popup;
