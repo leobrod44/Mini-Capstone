fa569298f28127ef1557189479527158f5848218
Dominique Isbister <97750045+Dom-Isbis@users.noreply.github.com>
2024-02-16 12:23:41 -0500

    Adding condo mgmt component
    Adding a condo component for the management companies. component displays as presented in the UI prototypes.

--- mini-cap/src/components/CondoMGMTComponent.jsx
+++ mini-cap/src/components/CondoMGMTComponent.jsx
@@ -2,23 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import "../styling/CondoComponent.css";
 
-const CondoComponent = ({ name, profilePicture, address, unitNumber, parkingSpot, locker, userType }) => {
-    
-    const userTypeClass = userType === 'Owner' ? 'owner' : 'renter';
+const CondoComponent = ({ name, profilePicture, address, unitNumber, parkingSpot, locker }) => {
 
     return (
         <div className="condo-details-container">
             <div className="condo-info">
                 <div className='condo-name-user-tag'>
                     <h2>{name}</h2>
-                    <div className={`user-tag ${userTypeClass}`}>{userType}</div>
                 </div>
                 
                 {profilePicture && <img src={profilePicture} alt="Profile" className="profile-picture" />}
                 <p>{address}</p>
                 <p>Unit Number: {unitNumber}</p>
-                {parkingSpot && <p>Parking Spot: {parkingSpot}</p>}
-                
+                <div className='parking-send-key'> 
+                    {parkingSpot && <p>Parking Spot: {parkingSpot}</p>}
+                    <button className="details-button">Send Key</button>
+                </div>
                 <div className='locker-details'>
                     {locker && <p>Locker: {locker}</p>}
                     <button className="details-button">Details</button>
@@ -34,8 +33,7 @@ CondoComponent.propTypes = {
     address: PropTypes.string.isRequired,
     unitNumber: PropTypes.string.isRequired,
     parkingSpot: PropTypes.string,
-    locker: PropTypes.string,
-    userType: PropTypes.string.isRequired
+    locker: PropTypes.string
 };
 
 export default CondoComponent;
--- mini-cap/src/pages/Dashboard.jsx
+++ mini-cap/src/pages/Dashboard.jsx
@@ -1,6 +1,6 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import CondoComponent from '../components/CondoComponent.jsx';
+import CondoComponent from "../components/CondoComponent.jsx";
 
 const Dashboard =() => {
 
