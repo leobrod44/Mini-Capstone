da8b2dea2131695ea455c147feeeb2dd8b1eb4d4
Yason287 <78317885+Yason287@users.noreply.github.com>
2024-02-18 22:56:58 -0500

    PropertyPage

--- mini-cap/src/App.js
+++ mini-cap/src/App.js
@@ -1,28 +1,29 @@
-import './App.css';
+import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import UserProfile from './pages/UserProfile';
-import LandingPage from './pages/LandingPage';
+import UserProfile from "./pages/UserProfile";
+import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 import Dashboard from "./pages/Dashboard";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import MGMTDashboard from './pages/MGMTDashboard';
+import MGMTDashboard from "./pages/MGMTDashboard";
+import PropertyPage from "./pages/PropertyPage";
 
 function App() {
-
   return (
     <BrowserRouter>
-       <ToastContainer pauseOnHover={false} autoClose={1000} />
-    <Routes>
-    <Route path="/" element={<LandingPage />} />
+      <ToastContainer pauseOnHover={false} autoClose={1000} />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
 
-    <Route path="/user-profile" element={<UserProfile/>} />
-    <Route path="/login" element={<LoginPage />} />
-    <Route path="/signup" element={<SignupPage />} />
-    <Route path="/dashboard" element={<Dashboard />} />
-    <Route path="/mgmtdashboard" element={<MGMTDashboard />} />
-    </Routes>
+        <Route path="/user-profile" element={<UserProfile />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/mgmtdashboard" element={<MGMTDashboard />} />
+        <Route path="/propertypage" element={<PropertyPage />} />
+      </Routes>
     </BrowserRouter>
   );
 }
---
+++ mini-cap/src/components/BackArrowBtn.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "../styling/BackArrowBtn.css";
+
+const BackArrowBtn = () => {
+    const navigate = useNavigate();
+
+    const goBack = () => {
+        navigate(-1); // This function will navigate to the previous page
+    };
+
+    return (
+        <div className="floating_button1" role="button" onClick={goBack}>
+            <div className="button_circle1">
+                <span className="back_arrow1">&larr; </span>
+            </div>
+        </div>
+    );
+};
+
+export default BackArrowBtn;
