67b77792f92b11f3c540085213c3990675d69612
Leo Brodeur <leobrod44@gmail.com>
2024-02-24 12:33:30 -0500

    Fixed bug where users dont see navbar

--- mini-cap/src/backend/Fetcher.js
+++ mini-cap/src/backend/Fetcher.js
@@ -263,23 +263,6 @@ export async function setPicture(data, path){
     }
 }
 
-
-export async function getCompanyData(email) {
-    
-        try {
-            const docRef = doc(db, "Company", email);
-            const docSnap = await getDoc(docRef);
-    
-            if (docSnap.exists()) {
-                return docSnap.data();
-            } else {
-                console.log("No such document!");
-            }
-    
-        } catch (err) {
-            console.error(err);
-        }
-    }
 export async function addCondo(data, propertyID){
     var pictureData = data.picture;
 
