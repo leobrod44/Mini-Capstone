eaa40585450c4ec138a6709ae3fe1d9c8e87b087
lauren rigante <93160873+laurenrigante@users.noreply.github.com>
2024-02-27 18:56:05 -0500

    adding import statements to fix  getstorage error

--- mini-cap/src/backend/ImageHandler.js
+++ mini-cap/src/backend/ImageHandler.js
@@ -1,6 +1,21 @@
 import { getStorage, uploadBytes, getDownloadURL, deleteObject, ref } from "firebase/storage";
 import emailjs from '@emailjs/browser';
+import {initializeApp} from "firebase/app";
+import {getFirestore} from "firebase/firestore";
 
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA9EVUOQ3j8g5W_lad6GeAK2CnT9oCXIVQ",
+    authDomain: "condoconnect-54ccc.firebaseapp.com",
+    projectId: "condoconnect-54ccc",
+    storageBucket: "condoconnect-54ccc.appspot.com",
+    messagingSenderId: "915299313041",
+    appId: "1:915299313041:web:a9832780ad51a84fb1e630",
+    measurementId: "G-MQJCJCX0ET"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 const storage = getStorage();
 const profilePictureRef = 'profilePictures/';
