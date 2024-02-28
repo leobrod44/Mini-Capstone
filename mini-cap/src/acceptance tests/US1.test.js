8b8747db9515989a9e717685131e9af7bac50949
layanat <tounsilayana@gmail.com>
2024-02-28 15:47:53 -0500

    Added acceptance test for US1 -frontend

---
+++ mini-cap/src/acceptance tests/US1.test.js
@@ -0,0 +1,245 @@
+import React from "react";
+import {
+  render,
+  screen,
+  fireEvent,
+  cleanup,
+  waitFor,
+} from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { toast } from "react-toastify";
+import SignupPage from "../pages/SignupPage";
+import { MANAGEMENT_COMPANY, RENTER_OWNER } from "../backend/Constants";
+
+
+jest.mock("react-toastify", () => {
+  const originalModule = jest.requireActual("react-toastify");
+  return {
+    ...originalModule,
+    toast: {
+      ...originalModule.toast,
+      error: jest.fn(),
+    },
+  };
+});
+
+describe("SignupPage ", () => {
+  it("should allow user to fill out the form and signup", async () => {
+    const { getByLabelText, getByText, getByRole } = render(
+      <Router>
+        <SignupPage />
+      </Router>
+    );
+
+    // Fill out the form
+    const roleDropdown = screen.getByLabelText("Select Role");
+    fireEvent.change(roleDropdown, { target: { value: RENTER_OWNER } });
+
+    fireEvent.change(getByLabelText("First Name"), {
+      target: { value: "John" },
+    });
+    fireEvent.change(getByLabelText("Last Name"), { target: { value: "Doe" } });
+    fireEvent.change(getByLabelText("Email"), {
+      target: { value: "john.doe@example.com" },
+    });
+    fireEvent.change(getByLabelText("Phone Number (optional)"), {
+      target: { value: "514-555-1234" },
+    });
+    fireEvent.change(getByLabelText("Password"), {
+      target: { value: "password123" },
+    });
+    fireEvent.change(getByLabelText("Confirm Password"), {
+      target: { value: "password123" },
+    });
+
+    const signupButton = getByText("Signup", { selector: "button" });
+
+    // Submit the form
+    fireEvent.click(signupButton);
+
+
+});
+it("should allow management company to fill out the form and signup", async () => {
+    const { getByLabelText, getByText, getByRole } = render(
+      <Router>
+        <SignupPage />
+      </Router>
+    );
+
+    // Fill out the form
+    const roleDropdown = screen.getByLabelText("Select Role");
+    fireEvent.change(roleDropdown, { target: { value: MANAGEMENT_COMPANY} });
+
+    fireEvent.change(getByLabelText("Company Name"), {
+      target: { value: "Condo" },
+    });
+    fireEvent.change(getByLabelText("Email"), {
+      target: { value: "condo@gmail.com" },
+    });
+    fireEvent.change(getByLabelText("Phone Number (optional)"), {
+      target: { value: "514-550-1217" },
+    });
+    fireEvent.change(getByLabelText("Password"), {
+      target: { value: "helloiamcompany123" },
+    });
+    fireEvent.change(getByLabelText("Confirm Password"), {
+      target: { value: "helloiamcompany123" },
+    });
+
+    const signupButton = getByText("Signup", { selector: "button" });
+
+    // Submit the form
+    fireEvent.click(signupButton);
+
+});
+it("should display error if wrong confirmed password is entered", async () => {
+    const { getByLabelText, getByText, getByRole, queryByText } = render(
+      <Router>
+        <SignupPage />
+      </Router>
+    );
+
+    // Fill out the form
+    const roleDropdown = screen.getByLabelText("Select Role");
+    fireEvent.change(roleDropdown, { target: { value: RENTER_OWNER } });
+
+    fireEvent.change(getByLabelText("First Name"), {
+      target: { value: "John" },
+    });
+    fireEvent.change(getByLabelText("Last Name"), { target: { value: "Doe" } });
+    fireEvent.change(getByLabelText("Phone Number (optional)"), {
+      target: { value: "514-555-1234" },
+    });
+    fireEvent.change(getByLabelText("Email"), {
+      target: { value: "john.doe@example.com" },
+    });
+    fireEvent.change(getByLabelText("Password"), {
+      target: { value: "password123" },
+    });
+    fireEvent.change(getByLabelText("Confirm Password"), {
+      target: { value: "password456" },
+    });
+
+    const signupButton = getByRole("button", { name: "Signup" });
+
+    // Submit the form
+    fireEvent.click(signupButton);
+
+    expect(toast.error).toHaveBeenCalledWith("Passwords do not match.");
+  });
+
+  it("should display error if password isnt valid", async () => {
+    const { getByLabelText, getByText, getByRole } = render(
+      <Router>
+        <SignupPage />
+      </Router>
+    );
+
+    // Fill out the form
+    const roleDropdown = screen.getByLabelText("Select Role");
+    fireEvent.change(roleDropdown, { target: { value: RENTER_OWNER } });
+    fireEvent.change(getByLabelText("First Name"), {
+      target: { value: "John" },
+    });
+    fireEvent.change(getByLabelText("Last Name"), { target: { value: "Doe" } });
+    fireEvent.change(getByLabelText("Phone Number (optional)"), {
+      target: { value: "514-555-1234" },
+    });
+    fireEvent.change(getByLabelText("Email"), {
+      target: { value: "john.doe@example.com" },
+    });
+    fireEvent.change(getByLabelText("Password"), { target: { value: "qwer" } });
+    fireEvent.change(getByLabelText("Confirm Password"), {
+      target: { value: "qwer" },
+    });
+
+    const signupButton = getByRole("button", { name: "Signup" });
+
+    // Submit the form
+    fireEvent.click(signupButton);
+
+    // Wait for toast message to appear
+    expect(toast.error).toHaveBeenCalledWith(
+      "Password must be at least 8 characters long."
+    );
+    fireEvent.change(getByLabelText("Password"), {
+      target: { value: "qwerqwerqwer" },
+    });
+    fireEvent.change(getByLabelText("Confirm Password"), {
+      target: { value: "qwerqwerqwer" },
+    });
+    fireEvent.click(signupButton);
+
+    expect(toast.error).toHaveBeenCalledWith(
+      "Password must contain both letters and numbers."
+    );
+  });
+  it("should display error if mandatory fields arent all entered", async () => {
+    const { getByLabelText, getByText, getByRole } = render(
+      <Router>
+        <SignupPage />
+      </Router>
+    );
+
+    // Fill out the form
+    const roleDropdown = screen.getByLabelText("Select Role");
+    fireEvent.change(roleDropdown, { target: { value: RENTER_OWNER } });
+    fireEvent.change(getByLabelText("First Name"), { target: { value: "" } });
+    fireEvent.change(getByLabelText("Last Name"), { target: { value: "" } });
+    fireEvent.change(getByLabelText("Phone Number (optional)"), {
+      target: { value: "" },
+    });
+    fireEvent.change(getByLabelText("Email"), {
+      target: { value: "john.doe@example.com" },
+    });
+    fireEvent.change(getByLabelText("Password"), { target: { value: "qwer" } });
+    fireEvent.change(getByLabelText("Confirm Password"), {
+      target: { value: "qwer" },
+    });
+
+    const signupButton = getByRole("button", { name: "Signup" });
+
+    // Submit the form
+    fireEvent.click(signupButton);
+    expect(toast.error).toHaveBeenCalledWith(
+      "Please fill in all mandatory fields."
+    );
+  });
+  it("should display error if email entered is invalid", async () => {
+    const { getByLabelText, getByText, getByRole } = render(
+      <Router>
+        <SignupPage />
+      </Router>
+    );
+
+    // Fill out the form
+    const roleDropdown = screen.getByLabelText("Select Role");
+    fireEvent.change(roleDropdown, { target: { value: RENTER_OWNER} });
+    fireEvent.change(getByLabelText("First Name"), {
+      target: { value: "John" },
+    });
+    fireEvent.change(getByLabelText("Last Name"), { target: { value: "Doe" } });
+    fireEvent.change(getByLabelText("Phone Number (optional)"), {
+      target: { value: "514-555-1234" },
+    });
+    fireEvent.change(getByLabelText("Email"), {
+      target: { value: "john.doeexamplecom" },
+    });
+    fireEvent.change(getByLabelText("Password"), {
+      target: { value: "password123" },
+    });
+    fireEvent.change(getByLabelText("Confirm Password"), {
+      target: { value: "password456" },
+    });
+
+    const signupButton = getByRole("button", { name: "Signup" });
+
+    // Submit the form
+    fireEvent.click(signupButton);
+
+    expect(toast.error).toHaveBeenCalledWith(
+      "Invalid email format. Please include '@' and '.' in your email address."
+    );
+  });
+});
