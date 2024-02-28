b893170e3b4d100e4cb01c330b5cba446b43f3cf
Matthew Flaherty <matthewflaherty77@hotmail.com>
2024-02-27 19:33:45 -0500

    fixed popup send key error

--- mini-cap/src/components/Popup_SendKey.js
+++ mini-cap/src/components/Popup_SendKey.js
@@ -4,7 +4,6 @@ import "../index.css";
 import { toast } from "react-toastify";
 import { storeCondoKey, sendCondoKey } from "../backend/PropertyHandler";
 import { checkEmailExists } from "../backend/UserHandler";
-import { sendCondoKey } from "../backend/PropertyHandler";
 import { RENTER_OWNER } from "../backend/Constants";
 
 const Popup_SendKey = ({ handleClose, condoId }) => {
