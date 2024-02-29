1d7f0d9197f5bb8dc332a32265dd2e3c56e5dd2d
lauren rigante <93160873+laurenrigante@users.noreply.github.com>
2024-02-29 18:20:34 -0500

    removing unused props to fix eslint error

--- mini-cap/src/components/CondoMGMTComponent.jsx
+++ mini-cap/src/components/CondoMGMTComponent.jsx
@@ -5,7 +5,7 @@ import  { useState} from "react";
 import Popup_SendKey from './Popup_SendKey';
 import { Link } from "react-router-dom";
 
-const CondoMgmtComponent = ({picture, unitNumber, parkingNumber, lockerNumber, property, squareFeet, unitPrice, unitSize, condoId}) => {
+const CondoMgmtComponent = ({picture, unitNumber, parkingNumber, lockerNumber, condoId}) => {
 
     const [showPopup, setShowPopup] = useState(false);
     const handlePopupToggle = () => {
