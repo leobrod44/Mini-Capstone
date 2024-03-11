7baa5f7c0e5539ba0cb68e064c1399a08722a90b
Yason287 <78317885+Yason287@users.noreply.github.com>
2024-03-10 21:57:06 -0400

    I just fixed the bug. Now, if the property is not created, the Condo Files button won't appear.

--- mini-cap/src/pages/PropertyDetailsPage.jsx
+++ mini-cap/src/pages/PropertyDetailsPage.jsx
@@ -52,12 +52,12 @@ const PropertyDetailsPage = () => {
         </div>
 
         <div >
-          <Link
+          {hasCondos && <Link
             className="details-button-condo-files"
             to={`/condo-files/${propertyID}/${propertyName}`}
           >
             Condo Files
-          </Link>
+          </Link>}
           {hasCondos ? (
             <div className="condo_list">
               {condoDetails.map((condo, index) => (
