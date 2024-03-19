82b79f61b9e4a8fc94ac78a0bb3a7e9326dd1497
Dominique Isbister <97750045+Dom-Isbis@users.noreply.github.com>
2024-03-19 17:03:34 -0400

    US15 - Moving the create request button #190
    Moving the create request button inside the condo request drop down.

--- mini-cap/src/components/RequestForm.jsx
+++ mini-cap/src/components/RequestForm.jsx
@@ -7,27 +7,13 @@ import 'react-toastify/dist/ReactToastify.css';
 import "../styling/RequestForm.css";
 import {getCondo} from "../backend/PropertyHandler";
 
-const RequestForm = ({handleClickClose}) => {
-    let { condoId } = useParams();
-    const [subject, setSubject] = useState("Administrative");
+const RequestForm = ({handleClickClose, condoInfo}) => {
+    let { condoID } = useParams();
+    const [subject, setSubject] = useState("");
     const [description, setDescription] = useState("");
     const [submitting, setSubmitting] = useState(false);
-    const [condoName, setCondoName] = useState("Condo Name");
-    const [unitNumber, setUnitNumber] = useState("Unit Number");
     const [notificationsActive, setNotificationsActive] = useState([]);
-
-    useEffect(() => {
-        const fetchCondo = async () => {
-            try {
-                const condo = await getCondo(condoId);
-                setCondoName(condo.propertyName);
-                setUnitNumber(condo.unitNumber);
-            } catch (err) {
-                console.error(err);
-            }
-        };
-        fetchCondo();
-    }, []);
+    const { propertyName, unitNumber } = condoInfo;
 
     useEffect(() => {
         const checkNotifications = () => {
@@ -58,12 +44,13 @@ const RequestForm = ({handleClickClose}) => {
                 toast.error("Please fill all fields");
                 return;
               }
-            const requestID = await submitRequest(condoId, subject, description);
+            const condo = await getCondo(condoID);
+            const requestID = await submitRequest(condoID, subject, description);
         } catch (error) {
             toast.error("Request failed to submit for unexpected reasons")
             console.error(error);
         } finally {
-            setSubject("Administrative");
+            setSubject("");
             setDescription("");
             setSubmitting(false); // Set submitting state back to false
         }
@@ -78,18 +65,18 @@ const RequestForm = ({handleClickClose}) => {
         <form className="form-box" onSubmit={handleSubmit}>
         <h2 className="titleform">Submit Request</h2>
         <br></br>
-        <h3 className="Requesttitle"> {condoName} {unitNumber} </h3>
+        <h3 className="Requesttitle"> {propertyName}: {unitNumber} </h3>
         <label className="dropdown" htmlFor="dropdown">Subject:</label>
         <select id="dropdown" value={subject} onChange={(e) => setSubject(e.target.value)}>
-            <option value="1">Moving Request</option>
-            <option value="1">Request Access</option>
-            <option value="1">Report Violation</option>
-            <option value="2">Report Damage in Common Area</option>
-            <option value="2">Request Maintenance</option>
-            <option value="1">General Question</option>
-            <option value="3">Payment Invoice</option>
-            <option value="3">Fee Inquiries</option>
-            <option value="3">Insurance Coverage Details</option>
+            <option value="1.1">Moving Request</option>
+            <option value="1.2">Request Access</option>
+            <option value="1.3">Report Violation</option>
+            <option value="2.1">Report Damage in Common Area</option>
+            <option value="2.2">Request Maintenance</option>
+            <option value="1.4">General Question</option>
+            <option value="3.1">Payment Invoice</option>
+            <option value="3.2">Fee Inquiries</option>
+            <option value="3.3">Insurance Coverage Details</option>
         </select>
         <label className="dropdown" htmlFor="description">Description:</label>
         <textarea id="description" name="description" value={description} onChange={(e) => setDescription(e.target.value)} placeholder="Enter request description here..."></textarea>
