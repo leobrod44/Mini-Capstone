1d5ea104da262081bd3dd2ffbd2812af44eda4e0
Alta Sheikh <98616697+altam-sh@users.noreply.github.com>
2024-03-20 00:47:51 -0400

    Delete and Edit Buttons Moved to Correct place #248
    Delete and Edit Buttons relocated to the bottom of the Details Component.

--- mini-cap/src/pages/CondoDetails.jsx
+++ mini-cap/src/pages/CondoDetails.jsx
@@ -307,15 +307,6 @@ export default function CondoDetails() {
 									)
 								)}
 							</div>
-
-							{/*NEED TO IMPLEMENT FUNCTIONALITY for edit*/}
-							<div>
-								{role === MANAGEMENT_COMPANY && (
-									<>
-										<button className="edit-button"> Edit</button>
-										<button className="delete-button" data-testid="delete-button-test" onClick={() => handleClickDelete()}>Delete</button>
-									</>)}
-							</div>
 							<div style={{display: "flex" , alignItems: "center"}}>
 							<h5 style={{paddingTop:"25px",  paddingBottom:"5%", paddingLeft:"25%", color:"#2f2c9", marginRight:"auto"}}>My financial details</h5>
 								<div>
@@ -328,6 +319,15 @@ export default function CondoDetails() {
 								{showFinancialDetails && (
 									<FinancialDetails/>
 								)}
+							</div>
+
+							{/*NEED TO IMPLEMENT FUNCTIONALITY for edit*/}
+							<div>
+								{role === MANAGEMENT_COMPANY && (
+								<>
+								<button className="edit-button"> Edit</button>
+								<button className="delete-button" data-testid="delete-button-test" onClick={() => handleClickDelete()}>Delete</button>
+								</>)}
 							</div>
 						</div>
 					</div>
