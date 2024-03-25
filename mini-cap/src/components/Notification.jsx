700028fb4291408175c763ef0c21f6b5e9411450
lauren rigante <93160873+laurenrigante@users.noreply.github.com>
2024-03-25 16:53:35 -0400

    editing functionality of see all button to expand modal and display older notifs #280

--- mini-cap/src/components/Notification.jsx
+++ mini-cap/src/components/Notification.jsx
@@ -1,35 +1,59 @@
 import "../styling/Notification.css";
 import { IoIosNotifications } from "react-icons/io";
-import React, { useState,useRef,useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import NotificationElement from "./NotificationElement";
 
 const Notification = () => {
   const [notifications, setNotifications] = useState([]);
   const [open, setOpen] = useState(false); // State for managing dropdown menu visibility
   const menuRef = useRef(); // Ref for the menu for handling click outside to close the menu
+  const [showAll, setShowAll] = useState(false);
 
-   // Constant list of notifications for testing purposes,
- const constantNotifications = [
-  {
-    condoName: "Unit XYZ",
-    notifMsg: "Maintenance Request",
-    dateTime: "2024-03-24 10:00 AM",
-    clicked: false
-  },
-  {
-    condoName: "Unit A456",
-    notifMsg: "Tax Form Request",
-    dateTime: "2024-03-18 9:00 AM",
-    clicked: false
-  },
-  {
-    condoName: "Unit B64",
-    notifMsg: "Report Damage",
-    dateTime: "2024-03-12 11:00 PM",
-    clicked: true
-  }
-];
-
+  // Constant list of notifications for testing purposes,
+  const constantNotifications = [
+    {
+      condoName: "Unit XYZ",
+      notifMsg: "Maintenance Request",
+      dateTime: "2024-03-24 10:00 AM",
+      clicked: false,
+    },
+    {
+      condoName: "Unit A456",
+      notifMsg: "Tax Form Request",
+      dateTime: "2024-03-18 9:00 AM",
+      clicked: false,
+    },
+    {
+      condoName: "Unit B64",
+      notifMsg: "Report Damage",
+      dateTime: "2024-03-12 11:00 PM",
+      clicked: true,
+    },
+    {
+      condoName: "Unit A456",
+      notifMsg: "Tax Form Request",
+      dateTime: "2024-03-18 9:00 AM",
+      clicked: false,
+    },
+    {
+      condoName: "Unit B64",
+      notifMsg: "Report Damage",
+      dateTime: "2024-03-12 11:00 PM",
+      clicked: true,
+    },
+    {
+      condoName: "Unit A456",
+      notifMsg: "Tax Form Request",
+      dateTime: "2024-03-18 9:00 AM",
+      clicked: false,
+    },
+    {
+      condoName: "Unit B64",
+      notifMsg: "Report Damage",
+      dateTime: "2024-03-12 11:00 PM",
+      clicked: true,
+    },
+  ];
 
   const toggleMenu = () => {
     console.log("Current state before toggle:", open);
@@ -41,13 +65,12 @@ const Notification = () => {
     console.log("Current state:", open);
     document.addEventListener("click", handleClickOutside);
     setNotifications(constantNotifications);
-    
+
     // Remove event listener when the component unmounts
     return () => {
       document.removeEventListener("click", handleClickOutside);
     };
   }, [open]); // Log the state whenever it changes
-
 
   // Function to handle clicking outside the modal
   const handleClickOutside = (event) => {
@@ -55,7 +78,6 @@ const Notification = () => {
       setOpen(false); // Close the modal if clicked outside
     }
   };
-
 
   // Function to handle clicking on a notification
   const handleNotificationClickInsideModal = (notification) => {
@@ -68,52 +90,55 @@ const Notification = () => {
 
   // Function to handle clicking on "See All" button
   const handleSeeAllInsideModal = () => {
-    // Navigate to a new page that displays all notifications
+    setShowAll((prevState) => !prevState); // Toggle the showAll state
   };
-
-
 
   /**
    * Functional component representing the notification icon.
    * @returns {JSX.Element} - The JSX for the notification icon.
    */
   return (
-<div className="notification-container" ref={menuRef}>
-    <div className="notif-wrapper">
-      <IoIosNotifications
-        className="notif"
-        data-testid="notification-icon"
-        onClick={toggleMenu}
-      />
+    <div className="notification-container" ref={menuRef}>
+      <div className="notif-wrapper">
+        <IoIosNotifications
+          className="notif"
+          data-testid="notification-icon"
+          onClick={toggleMenu}
+        />
       </div>
-        <div className={`myDropdownNotif ${open ? 'active' : 'inactive'}`}>
-          <div className="notification-header">
-            <h2 className="notif-title">Notifications</h2>
-          </div>
-           {notifications.length > 0 ? ( // Check if there are notifications
-            <div className="notification-list">
-              {notifications.map((notification, index) => (
+      <div className={`myDropdownNotif ${open ? "active" : "inactive"}`}>
+        <div className="notification-header">
+          <h2 className="notif-title">Notifications</h2>
+        </div>
+        {notifications.length > 0 ? ( // Check if there are notifications
+          <div className="notification-list">
+            {notifications
+              .slice(0, showAll ? notifications.length : 3)
+              .map((notification, index) => (
                 <NotificationElement
                   key={index}
                   notification={notification}
-                  onClick={() => handleNotificationClickInsideModal(notification)}
-                  onClear={() => handleClearNotificationInsideModal(notification)}
+                  onClick={() =>
+                    handleNotificationClickInsideModal(notification)
+                  }
+                  onClear={() =>
+                    handleClearNotificationInsideModal(notification)
+                  }
                 />
               ))}
-            </div>
-          ) : (
-            <div className="no-notifications-msg">No new notifications.</div>
-          )}
-         
-          <div className="btn-div-notif"> 
-          <button className="See-all" onClick={handleSeeAllInsideModal}>See All</button>
           </div>
+        ) : (
+          <div className="no-notifications-msg">No new notifications.</div>
+        )}
+
+        <div className="btn-div-notif">
+          <button className="See-all" onClick={handleSeeAllInsideModal}>
+            {showAll ? "Hide" : "See All"}
+          </button>
         </div>
-        
+      </div>
     </div>
   );
 };
 
-
-  
 export default Notification;
