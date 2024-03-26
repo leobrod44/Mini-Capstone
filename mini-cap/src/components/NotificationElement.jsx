c14324a2957ccb001ca63b6a6ae48166d81b066c
lauren rigante <93160873+laurenrigante@users.noreply.github.com>
2024-03-26 11:09:18 -0400

    adding function to reformat datetime #280

--- mini-cap/src/components/NotificationElement.jsx
+++ mini-cap/src/components/NotificationElement.jsx
@@ -2,20 +2,33 @@ import React from "react";
 import { IoIosClose, IoIosWarning } from "react-icons/io";
 import "../styling/NotificationElement.css";
 
-const NotificationElement = ({ notification, onClick, onClear }) => {
-  const { condoName, notifMsg, dateTime, clicked } = notification;
 
+const formatDate = (datetime) => {
+  const date = new Date(datetime);
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  return `${year}-${month}-${day} ${hours}:${minutes}`;
+};
+
+
+const NotificationElement = ({ notification, onClick, onClear }) => {
+  const { condoName, message, date, viewed } = notification;
+
+  const formattedDate = formatDate(date);
   return (
     <div
-      className={`notification-element ${clicked ? 'clicked' : 'not-clicked'}`}
+      className={`notification-element ${viewed ? 'clicked' : 'not-clicked'}`}
       onClick={onClick}
     >
       <div className="notification-info">
-        <div className="notifMsg">{notifMsg}</div>
+        <div className="notifMsg">{message}</div>
       </div>
       <div className="icon-date-wrapper">
-      {clicked ? null : <IoIosWarning className="exclamation-icon" />}
-      <div className="date-time">{dateTime}</div>
+      {viewed ? null : <IoIosWarning className="exclamation-icon" />}
+      <div className="date-time">{formattedDate}</div>
       </div>
       <div className="notification-details">
         <div className="unitNumber">{condoName}</div>
