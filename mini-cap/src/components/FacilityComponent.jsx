ce9fff8eb2bce1da4be80946a109b7943ec9c4e6
Alta Sheikh <98616697+altam-sh@users.noreply.github.com>
2024-03-27 22:57:45 -0400

    Add Props for Backend Connection in Facility Component #304
    added the props, variable names will be subject to changed based on decisions in backend

--- mini-cap/src/components/FacilityComponent.jsx
+++ mini-cap/src/components/FacilityComponent.jsx
@@ -3,15 +3,19 @@ import PropTypes from 'prop-types';
 import "../styling/FacilityComponent.css";
 import  { useState } from "react";
 
-const FacilityComponent = () => {
+const FacilityComponent = ({facilityTitle, facilityDescription}) => {
 
     return (
         <div className="component-container">
-            <div className="facility-title">Facility Component</div>
-            <div className="facility-description">Little Facility Description</div>
+            <div className="facility-title">Facility Component{facilityTitle}</div>
+            <div className="facility-description">Little Facility Description{facilityDescription}</div>
             <button className='make-reservation-button'>Make Reservation</button>
         </div>
     );
 };
 
+FacilityComponent.propTypes = {
+    facilityTitle: PropTypes.string,
+    facilityDescription: PropTypes.string,
+};
 export default FacilityComponent;
