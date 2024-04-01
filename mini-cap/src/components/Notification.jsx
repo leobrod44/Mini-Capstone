905b0602bd2e2be301afaed933f358cae6a5e696
Dominique Isbister <97750045+Dom-Isbis@users.noreply.github.com>
2024-04-01 14:47:18 -0400

    userID update #310
    Using useParams to fetch userID and adding documentation to it.

--- mini-cap/src/components/Notification.jsx
+++ mini-cap/src/components/Notification.jsx
@@ -2,6 +2,7 @@ import "../styling/Notification.css"
 import { IoIosNotifications } from "react-icons/io";
 import React, { useState, useEffect, useRef } from "react";
 import { getNotifications } from "../backend/RequestHandler";
+import { useParams } from "react-router-dom";
 
 const Notification = () => {
     /**
@@ -15,6 +16,12 @@ const Notification = () => {
      * @type {[Array<NotificationData>, React.Dispatch<React.SetStateAction<Array<NotificationData>>>]}
      */
     const [notifications, setNotifications] = useState([]);
+
+    /**
+     * Destructures the `userID` parameter from the URL parameters.
+     * @type {object}
+     */
+    let { userID } = useParams();
   
     /**
      * Effect hook to fetch notifications for the current user when the component mounts.
@@ -24,13 +31,13 @@ const Notification = () => {
       // Fetch notifications for the current user
       const fetchNotifications = async () => {
         try {
-          const userID = "currentUserId"; // Replace with actual user ID
-          const fetchedNotifications = await getNotifications(userID);
-          const unviewedNotifications = fetchedNotifications.filter(notification => !notification.viewed);
-          setNotifications(unviewedNotifications);
-          setUnviewedCount(unviewedNotifications.length);
+            
+            const fetchedNotifications = await getNotifications(userID);
+            const unviewedNotifications = fetchedNotifications.filter(notification => !notification.viewed);
+            setNotifications(unviewedNotifications);
+            setUnviewedCount(unviewedNotifications.length);
         } catch (error) {
-          console.error('Error fetching notifications:', error);
+            console.error('Error fetching notifications:', error);
         }
       };
   
