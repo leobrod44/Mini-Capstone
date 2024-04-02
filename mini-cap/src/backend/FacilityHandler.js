dccd0f320127c380a7ea982311bd2cfe91bac410
Leo Brodeur <leobrod44@gmail.com>
2024-04-02 17:33:26 -0400

    Added making reservations and getting reservations of a month #320

--- mini-cap/src/backend/FacilityHandler.js
+++ mini-cap/src/backend/FacilityHandler.js
@@ -28,13 +28,14 @@ export async function addFacility(facility) {
         }
         facility.dailyAvailabilities = [1]*24/blockSize;
         facility.blockSize = blockSize;
-        await addDoc(amenitiesColl, {
+        var docRef = await addDoc(amenitiesColl, {
             propertyID: facility.propertyID,
             type: facility.title,
             description: facility.description,
             dailyAvailabilities: facility.dailyAvailabilities,
             blockSize: facility.blockSize
         });
+
     } catch (e) {
         throw e;
     }
@@ -43,14 +44,45 @@ export async function addFacility(facility) {
 
 export async function makeReservation(reservation){
     try{
-        const userRef = doc(db, "Users", reservation.userID);
-        const reservationRef = collection(userRef, "Reservations");
-        const propertyRef = doc(userRef, "Properties", reservation.propertyID);
-        const facilityRef = doc(propertyRef, "Facilities", reservation.facilityID);
-
-        await addDoc(reservationRef, reservation);
+        var month = reservation.month;
+        const propertyRef = doc(db, "Property", reservation.propertyID);
+        // Retrieve the collection of amenities from the property
+        const facilityRef = collection(propertyRef, "Facilities");
+        const facility = doc(facilityRef, reservation.facilityID);
+        const monthCollectionRef = collection(facility, month+"");
+        // Add a placeholder document in the month collection if needed
+        await addDoc(monthCollectionRef, {
+            date: reservation.date,
+            startTime: reservation.startTime,
+            endTime: reservation.endTime,
+            userID: reservation.userID
+        });
     } catch (e) {
         throw e;
     }
+}
+export async function getMonthlyReservations(propertyID,facilityID,month){
+    try {
+        const propertyRef = doc(db, "Property", propertyID);
+        // Retrieve the collection of amenities from the property
+        const facilityRef = collection(propertyRef, "Facilities");
+        const facility = doc(facilityRef, facilityID);
+        const monthCollectionRef = collection(facility, month+"");
+        const monthCollection = await getDocs(monthCollectionRef);
+        var reservations = {};
+        monthCollection.forEach(doc => {
+            if(reservations.hasOwnProperty(doc.data().date)){
+                reservations[doc.data().date].push(doc.data().startTime);
+            }
+            else{
+                reservations[doc.data().date] = [doc.data().startTime];
+            }
+        });
+        
+        return reservations;
+    } catch (e) {
+        throw e;
+    }
+
 }
 
