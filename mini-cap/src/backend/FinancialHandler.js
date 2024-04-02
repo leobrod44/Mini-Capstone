b72033ea3620d9e68da73ae0ee3eb14d6186206b
SenecalRenaud <senecalrenaud@gmail.com>
2024-04-02 17:56:09 -0400

    Merge main into branch #314

--- mini-cap/src/backend/FinancialHandler.js
+++ mini-cap/src/backend/FinancialHandler.js
@@ -11,59 +11,8 @@ emailjs.init({
     publicKey: "Gw4N_w4eDx939VEBl",
 });
 import { jsPDF } from "jspdf";
-import {calculateCondoFees, getPropertyData} from "./PropertyHandler";
+import {getPropertyData} from "./PropertyHandler";
 
-
-export async function generateFinancialReport(condoID) {
-    try{
-        //pull from main after financialhandler changes (calculateCondoFees changed files)
-        const financialDetails = await calculateCondoFees(condoID);
-        //const rentPaid = await checkRentPaid(condoID);
-
-        const docRef = doc(db, "Condo", condoID);
-        // Fetch the snapshot of the condo document
-        const docSnap = await getDoc(docRef);
-
-        // Extract condo data from the snapshot
-        const condoData = docSnap.data();
-        const propertyData = await getPropertyData(condoData.property);
-
-        const currentDate = new Date();
-        const year = currentDate.getFullYear();
-        const month = String(currentDate.getMonth() + 1).padStart(2, '0'); // Months are zero-based
-        const day = String(currentDate.getDate()).padStart(2, '0');
-        const formattedDate = `${year}/${month}/${day}`;
-        const monthName = currentDate.toLocaleString('default', { month: 'long' });
-
-        const document = new jsPDF();
-
-        // Header
-        document.setFontSize(18);
-        document.text(propertyData['propertyName'] + " Unit " + condoData.unitNumber + " Financial Report", 10, 20);
-        document.setFontSize(14);
-        document.text(propertyData['address'], 10, 30);
-        document.setFontSize(14);
-        document.text('Generated on: ' + formattedDate, 10, 40);
-
-        // Line
-        document.setLineWidth(0.5);
-        document.line(10, 45, 200, 45);
-
-        // Data
-        document.setFontSize(12);
-        //********* a changer pour rentPaid
-        const rentPaidString = monthName.toString() + "'s rent paid: " + (false ? 'Yes' : 'No');
-        const amountString = false ? 'paid: ' : 'to be paid: ';
-        document.text(rentPaidString, 10, 55);
-        document.text('Total amount ' + amountString + financialDetails['totalPrice'] + '$', 10, 65);
-        document.text('- Rent amount ' + amountString + financialDetails['rent'] + '$', 15, 75);
-        document.text('- Amenities amount ' + amountString + financialDetails['amenitiesPrice'] + '$', 15, 85);
-
-        document.save(`${year}-${month}-${day}_` + propertyData['propertyName'] + "_" + condoData.unitNumber + "_Financial_Report.pdf");
-    }catch (error){
-        throw error;
-    }
-}
 
 /**
  * Updates the rentPaid field of a condominium to mark that rent has been paid.
@@ -83,6 +32,34 @@ export async function payRent(condoID) {
     } catch (error) {
         // If an error occurs during the process, throw an error with a descriptive message
         throw new Error(error);
+    }
+}
+
+/**
+ * Checks whether rent has been paid for a condominium.
+ * @param {string} condoID - The ID of the condominium.
+ * @returns {Promise<boolean>} A promise that resolves with a boolean indicating whether rent has been paid.
+ * @throws {Error} Throws an error if there was an issue checking the rentPaid field.
+ */
+export async function checkRentPaid(condoID) {
+    try {
+        // Get document reference for the specified condo ID
+        const condoDocRef = doc(db, "Condo", condoID);
+        // Fetch document snapshot
+        const condoDocSnap = await getDoc(condoDocRef);
+
+        // Check if condo document exists
+        if (condoDocSnap.exists) {
+            if (!condoDocSnap.data().hasOwnProperty("rentPaid"))
+                return false;
+            else
+                return condoDocSnap.data().rentPaid;
+        } else {
+            throw new Error("Condo doc not found in checkRentPaid");
+        }
+    } catch (error) {
+        // Throw error to propagate it up the call stack
+        throw error;
     }
 }
 
@@ -149,6 +126,55 @@ export async function calculateCondoFees(condoId) {
         }
     } catch (error) {
         // Rethrow the error to propagate it up the call stack
+        throw error;
+    }
+}
+
+export async function generateFinancialReport(condoID) {
+    try{
+        const financialDetails = await calculateCondoFees(condoID);
+        const rentPaid = await checkRentPaid(condoID);
+
+        const docRef = doc(db, "Condo", condoID);
+        // Fetch the snapshot of the condo document
+        const docSnap = await getDoc(docRef);
+
+        // Extract condo data from the snapshot
+        const condoData = docSnap.data();
+        const propertyData = await getPropertyData(condoData.property);
+
+        const currentDate = new Date();
+        const year = currentDate.getFullYear();
+        const month = String(currentDate.getMonth() + 1).padStart(2, '0'); // Months are zero-based
+        const day = String(currentDate.getDate()).padStart(2, '0');
+        const formattedDate = `${year}/${month}/${day}`;
+        const monthName = currentDate.toLocaleString('default', { month: 'long' });
+
+        const document = new jsPDF();
+
+        // Header
+        document.setFontSize(18);
+        document.text(propertyData['propertyName'] + " Unit " + condoData.unitNumber + " Financial Report", 10, 20);
+        document.setFontSize(14);
+        document.text(propertyData['address'], 10, 30);
+        document.setFontSize(14);
+        document.text('Generated on: ' + formattedDate, 10, 40);
+
+        // Line
+        document.setLineWidth(0.5);
+        document.line(10, 45, 200, 45);
+
+        // Data
+        document.setFontSize(12);
+        const rentPaidString = monthName.toString() + "'s rent paid: " + (rentPaid ? 'Yes' : 'No');
+        const amountString = rentPaid ? 'paid: ' : 'to be paid: ';
+        document.text(rentPaidString, 10, 55);
+        document.text('Total amount ' + amountString + financialDetails['totalPrice'] + '$', 10, 65);
+        document.text('- Rent amount ' + amountString + financialDetails['rent'] + '$', 15, 75);
+        document.text('- Amenities amount ' + amountString + financialDetails['amenitiesPrice'] + '$', 15, 85);
+
+        document.save(`${year}-${month}-${day}_` + propertyData['propertyName'] + "_" + condoData.unitNumber + "_Financial_Report.pdf");
+    }catch (error){
         throw error;
     }
 }
