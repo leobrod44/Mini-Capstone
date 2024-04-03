245a5eca0ac6f43f3e151c0ca468e79e9ff7a3c5
Leo Brodeur <leobrod44@gmail.com>
2024-04-03 13:03:07 -0400

    Added reservation fetching and updating from property #316

--- mini-cap/src/backend/FacilityHandler.js
+++ mini-cap/src/backend/FacilityHandler.js
@@ -1,6 +1,7 @@
 import {collection, doc,addDoc, getDoc, getDocs, getFirestore, updateDoc} from "firebase/firestore";
 import {initializeApp} from "firebase/app";
 import {firebaseConfig} from "./FirebaseConfig";
+import { getPropertyData, getUserCondos } from "./PropertyHandler";
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
@@ -51,12 +52,27 @@ export async function makeReservation(reservation){
         const facility = doc(facilityRef, reservation.facilityID);
         const monthCollectionRef = collection(facility, month+"");
         // Add a placeholder document in the month collection if needed
-        await addDoc(monthCollectionRef, {
+        var docRef = await addDoc(monthCollectionRef, {
             date: reservation.date,
             startTime: reservation.startTime,
             endTime: reservation.endTime,
             userID: reservation.userID
         });
+        var userRef = doc(db, "Users", reservation.userID);
+        var userDoc = await getDoc(userRef);
+        var data = userDoc.data()   
+        var path = `Property/${reservation.propertyID}/Facilities/${reservation.facilityID}/${month}/${docRef.id}`
+        if(data.hasOwnProperty("reservations")){
+            await updateDoc(userRef, {
+                reservations: [...userDoc.data().reservations,path]
+            });
+        }
+        else{
+            await updateDoc(userRef, {
+                reservations: [path]
+            });
+        }
+        
     } catch (e) {
         throw e;
     }
@@ -70,7 +86,7 @@ export async function getMonthlyReservations(propertyID,facilityID,month){
         const monthCollectionRef = collection(facility, month+"");
         const monthCollection = await getDocs(monthCollectionRef);
         var reservations = {};
-        monthCollection.forEach(doc => {
+        monthCollection.forEach(async doc => {
             if(reservations.hasOwnProperty(doc.data().date)){
                 reservations[doc.data().date].push(doc.data().startTime);
             }
@@ -78,11 +94,81 @@ export async function getMonthlyReservations(propertyID,facilityID,month){
                 reservations[doc.data().date] = [doc.data().startTime];
             }
         });
-        
+
         return reservations;
     } catch (e) {
         throw e;
     }
 
 }
+export async function getFacilities(propertyID){
+    try {
+        const propertyRef = doc(db, "Property", propertyID);
+        // Retrieve the collection of amenities from the property
+        const facilityRef = collection(propertyRef, "Facilities");
+        const facilityCollection = await getDocs(facilityRef);
+        var facilities = [];
+        facilityCollection.forEach(doc => {
+            facilities.push(doc.data());
+        });
+        return facilities;
+    } catch (e) {
+        throw e;
+    }
+}
 
+export async function getPropertiesJoinReservationAndFacilities(userID){
+    try {
+        //var userID = "leo@gmail.com"
+        const userRef = doc(db, "Users", userID);
+        const userDoc = await getDoc(userRef);
+        const condos = await getUserCondos(userID);
+        var propertiesArray = await Promise.all(condos.map(async (condo) => {
+            var prop = await getPropertyData(condo.property);
+            prop.id = condo.property;
+            return prop;
+        }));
+        const uniquePropertyArray = Array.from(new Set(propertiesArray.map(JSON.stringify))).map(JSON.parse);
+        const reservations = userDoc.data().reservations;
+        var propertiesData = await Promise.all(uniquePropertyArray.map(async (property) => {
+            property.facilities = await getFacilities(property.id);
+            property.reservations = await Promise.all(reservations.map(async (reservation) => {
+                if(reservation.split("/")[1]==property.id){
+                    var docu = await getDoc(doc(db,reservation))
+                    return docu.data();
+                }
+            }));
+            property.reservations = property.reservations.filter(Boolean);
+            return property;
+        }));
+
+        return propertiesData;
+
+    } catch (e) {
+        throw e;
+    }
+}
+export async function getReservationUpdates(userID){
+    try {
+        const userRef = doc(db, "Users", userID);
+        const userDoc = await getDoc(userRef);
+        const reservations = userDoc.data().reservations;
+        var reservationsData = await Promise.all(reservations.map(async (reservation) => {
+            var docu = await getDoc(doc(db,reservation))
+            if(docu.data().date >= new Date()){
+                var facility = await getDoc(doc(db, "Facility", docu.data().facilityID));
+                const newNotification = {
+                    message: reservation.startTime + "-"+reservation.endTime,
+                    path: "my-reservations",
+                    date: reservation.date,
+                    type: facility.data().type,
+                    viewed: false
+                }
+                return newNotification;
+            }
+        }));
+        return reservationsData;
+    } catch (e) {
+        throw e;
+    }
+}
