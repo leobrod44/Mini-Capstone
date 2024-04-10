58e67b85749f4b9a27a9c385711a43d23db1d371
lauren rigante <93160873+laurenrigante@users.noreply.github.com>
2024-04-09 21:06:58 -0400

    adding and fixing reservations slots so theya re consistent #322

--- mini-cap/src/pages/CalendarPage.jsx
+++ mini-cap/src/pages/CalendarPage.jsx
@@ -108,10 +108,14 @@ const CalendarPage = ({ totalAvailableSlots }) => {
         const reservedTimeSlotsForDate = reservations[slotDate.getDate()] || [];
         // TODO: will this always be hard coded?
         const defaultTimeSlots = [
+            "08:00 AM - 09:00 AM",
             "09:00 AM - 10:00 AM",
-            "10:30 AM - 11:30 AM",
+            "10:00 AM - 11:00 AM",
+            "11:00 AM - 12:00 PM",
             "01:00 PM - 02:00 PM",
+            "02:00 PM - 03:00 PM",
             "03:00 PM - 04:00 PM",
+            "04:00 PM - 05:00 PM",
         ];
 
         // Filter available time slots based on reserved time slots for the selected date
