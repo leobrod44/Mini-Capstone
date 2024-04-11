d2017dcd4867f554ddee5a01a6a8d1b333981dc6
Alta Sheikh <98616697+altam-sh@users.noreply.github.com>
2024-04-11 09:57:53 -0400

    Add Documentation to ReservationComponent.jsx #322

--- mini-cap/src/components/ReservationComponent.jsx
+++ mini-cap/src/components/ReservationComponent.jsx
@@ -1,11 +1,21 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import "../styling/FacilityComponent.css";
 import  { useState } from "react";
 import DeleteModal from "../components/DeleteModal";
-import { propTypes } from 'react-bootstrap/esm/Image';
 
+/**
+ * ReservationComponent displays information about a reservation.
+ * @param {object} props - The properties passed to the component.
+ * @param {string} props.facilityType - The type of facility for the reservation.
+ * @param {string} props.startTime - The start time of the reservation.
+ * @param {string} props.endTime - The end time of the reservation.
+ * @param {string} props.date - The date of the reservation.
+ * @param {number} props.month - The month index (0-11) of the reservation.
+ * @returns {JSX.Element} A React component displaying reservation information.
+ */
 const ReservationComponent = ({facilityType, startTime, endTime, date, month}) => {
+    // Array of month names for displaying the month of the reservation
     const monthNames = [
         "January", "February", "March", "April", "May", "June",
         "July", "August", "September", "October", "November", "December"
@@ -16,21 +26,24 @@ const ReservationComponent = ({facilityType, startTime, endTime, date, month}) =
 
     return (
         <div className="component-container">
+            {/* Display the facility title */}
             <div className="facility-title">Upcoming Reservation: {facilityType}</div>
             <div className="facility-description"></div>
+            {/* Display the date of the reservation */}
             <div>Date: {reservationMonth} {date} </div>
+            {/* Display the time range of the reservation */}
             <div>Time: {startTime} - {endTime}</div>
         </div>
-        
     );
 };
 
+// PropTypes for type checking the props passed to ReservationComponent
 ReservationComponent.propTypes = {
-    facilityTitle: PropTypes.string,
-    date: PropTypes.string,
-    month: PropTypes.number,
-    startTime: PropTypes.string,
-    endTime: PropTypes.string
+    facilityType: PropTypes.string.isRequired, // Facility type is a required string
+    date: PropTypes.string.isRequired, // Date is a required string
+    month: PropTypes.number.isRequired, // Month is a required number
+    startTime: PropTypes.string.isRequired, // Start time is a required string
+    endTime: PropTypes.string.isRequired // End time is a required string
 };
 
 export default ReservationComponent;
