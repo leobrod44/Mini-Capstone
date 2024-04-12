1a1ceef3dd00c5fe447ee5bddafb8b2aea8eac12
Zaheer Quraishi <zaheerqur1811@gmail.com>
2024-04-12 10:21:24 -0400

    83.33% coverage for PropertyFacilities

--- mini-cap/src/tests/PropertyFacilities.test.js
+++ mini-cap/src/tests/PropertyFacilities.test.js
@@ -1,50 +1,220 @@
 import React from "react";
-import { render, screen, fireEvent } from "@testing-library/react";
+import {
+  render,
+  screen,
+  fireEvent,
+  waitFor,
+  act,
+} from "@testing-library/react";
 import PropertyFacilities from "../pages/PropertyFacilities";
 import "@testing-library/jest-dom";
+import { MemoryRouter } from "react-router-dom";
+
+jest.mock("../backend/FacilityHandler.js", () => ({
+  getFacilities: jest.fn(),
+  editFacility: jest.fn(),
+  addFacility: jest.fn(),
+}));
+jest.mock("../backend/PropertyHandler.js", () => ({
+  getPropertyData: jest.fn(),
+}));
 
 describe("PropertyFacilities", () => {
-  test("renders the component and its elements", () => {
-    render(<PropertyFacilities />);
+  test("renders the component and its elements", async () => {
+    const mockFacilities = [
+      {
+        id: "1",
+        type: "Gym",
+        description: "Fitness equipment",
+        startHour: "08:00",
+        endHour: "22:00",
+      },
+      {
+        id: "2",
+        type: "Pool",
+        description: "Swimming pool",
+        startHour: "08:00",
+        endHour: "22:00",
+      },
+    ];
+    const FacilityHandler = require("../backend/FacilityHandler.js"); // Requiring the mocked module
+    FacilityHandler.getFacilities.mockResolvedValueOnce(mockFacilities); // Mocking facilities with data
+    const PropertyHandler = require("../backend/PropertyHandler.js"); // Requiring the mocked module
+    const mockpropertyName = { propertyName: "Test Property" };
+    PropertyHandler.getPropertyData.mockResolvedValueOnce(mockpropertyName); // Mocking facilities with data
 
+    await act(async () => {
+      render(
+        <MemoryRouter>
+          <PropertyFacilities />
+        </MemoryRouter>
+      );
+    });
     // Check if the title is rendered
-    expect(screen.getByText("Property Facilities")).toBeInTheDocument();
+    expect(screen.getByText("Test Property Facilities")).toBeInTheDocument();
 
     // Check if the Add Facility button is rendered
     expect(screen.getByText("Add Facility")).toBeInTheDocument();
 
-    // Check if the mock facility is rendered
-    expect(screen.getByText("Gym")).toBeInTheDocument();
-    expect(screen.getByText("Apes together... strong.")).toBeInTheDocument();
-    expect(screen.getByText("Hours: 06:00 - 22:00")).toBeInTheDocument();
-
-    // Check if the Edit and Delete buttons are rendered
-    expect(screen.getByText("Edit")).toBeInTheDocument();
-    expect(screen.getByText("Delete")).toBeInTheDocument();
+    // Asserting that the component renders with facilities
+    await waitFor(() => {
+      expect(screen.getByText("Gym")).toBeInTheDocument();
+    });
+    await waitFor(() => {
+      expect(screen.getByText("Pool")).toBeInTheDocument();
+    });
   });
+  test("renders the component and its elements, click edit and click on cancel", async () => {
+    const mockFacilities = [
+      {
+        id: "1",
+        type: "Gym",
+        description: "Fitness equipment",
+        startHour: "08:00",
+        endHour: "22:00",
+      },
+      {
+        id: "2",
+        type: "Pool",
+        description: "Swimming pool",
+        startHour: "08:00",
+        endHour: "22:00",
+      },
+    ];
+    const FacilityHandler = require("../backend/FacilityHandler.js"); // Requiring the mocked module
+    FacilityHandler.getFacilities.mockResolvedValueOnce(mockFacilities); // Mocking facilities with data
+    const PropertyHandler = require("../backend/PropertyHandler.js"); // Requiring the mocked module
+    const mockpropertyName = { propertyName: "Test Property" };
+    PropertyHandler.getPropertyData.mockResolvedValueOnce(mockpropertyName); // Mocking facilities with data
 
-  test("handles Add Facility button click", () => {
-    // You can mock the console.log to test the click event
-    const consoleSpy = jest.spyOn(console, "log");
-    render(<PropertyFacilities />);
+    await act(async () => {
+      render(
+        <MemoryRouter>
+          <PropertyFacilities />
+        </MemoryRouter>
+      );
+    });
+    // Check if the title is rendered
+    expect(screen.getByText("Test Property Facilities")).toBeInTheDocument();
 
-    const addButton = screen.getByText("Add Facility");
-    fireEvent.click(addButton);
+    // Check if the Add Facility button is rendered
+    expect(screen.getByText("Add Facility")).toBeInTheDocument();
 
-    expect(consoleSpy).toHaveBeenCalledWith("Add facility logic here");
+    // Asserting that the component renders with facilities
+    await waitFor(() => {
+      expect(screen.getByText("Gym")).toBeInTheDocument();
+    });
+    await waitFor(() => {
+      expect(screen.getByText("Pool")).toBeInTheDocument();
+    });
+
+    const editBtns = screen.getAllByText("Edit");
+    fireEvent.click(editBtns[0]);
+
+    const cancelBtn = screen.getByText("Cancel");
+    fireEvent.click(cancelBtn);
   });
+  test("renders the component and its elements, click edit and click on save", async () => {
+    const mockFacilities = [
+      {
+        id: "1",
+        type: "Gym",
+        description: "Fitness equipment",
+        startHour: "08:00",
+        endHour: "22:00",
+      },
+      {
+        id: "2",
+        type: "Pool",
+        description: "Swimming pool",
+        startHour: "08:00",
+        endHour: "22:00",
+      },
+    ];
+    const FacilityHandler = require("../backend/FacilityHandler.js"); // Requiring the mocked module
+    FacilityHandler.getFacilities.mockResolvedValueOnce(mockFacilities); // Mocking facilities with data
+    FacilityHandler.editFacility.mockResolvedValueOnce({});
+    const PropertyHandler = require("../backend/PropertyHandler.js"); // Requiring the mocked module
+    const mockpropertyName = { propertyName: "Test Property" };
+    PropertyHandler.getPropertyData.mockResolvedValueOnce(mockpropertyName); // Mocking facilities with data
 
-  test("handles Delete button click", () => {
-    // Mock the console.log to test the click event
-    const consoleSpy = jest.spyOn(console, "log");
-    render(<PropertyFacilities />);
+    await act(async () => {
+      render(
+        <MemoryRouter>
+          <PropertyFacilities />
+        </MemoryRouter>
+      );
+    });
+    // Check if the title is rendered
+    expect(screen.getByText("Test Property Facilities")).toBeInTheDocument();
 
-    const deleteButton = screen.getByText("Delete");
-    fireEvent.click(deleteButton);
+    // Check if the Add Facility button is rendered
+    expect(screen.getByText("Add Facility")).toBeInTheDocument();
 
-    // Assuming the mock facility's id is 1
-    expect(consoleSpy).toHaveBeenCalledWith("Delete facility logic here");
+    // Asserting that the component renders with facilities
+    await waitFor(() => {
+      expect(screen.getByText("Gym")).toBeInTheDocument();
+    });
+    await waitFor(() => {
+      expect(screen.getByText("Pool")).toBeInTheDocument();
+    });
+
+    const editBtns = screen.getAllByText("Edit");
+    fireEvent.click(editBtns[0]);
+
+    await waitFor(() => {
+      expect(screen.getByText("Save")).toBeInTheDocument();
+    });
+
+    const saveBtn = screen.getByText("Save");
+    fireEvent.click(saveBtn);
   });
+  test("renders the component and its elements, click edit Delete", async () => {
+    const mockFacilities = [
+      {
+        id: "1",
+        type: "Gym",
+        description: "Fitness equipment",
+        startHour: "08:00",
+        endHour: "22:00",
+      },
+      {
+        id: "2",
+        type: "Pool",
+        description: "Swimming pool",
+        startHour: "08:00",
+        endHour: "22:00",
+      },
+    ];
+    const FacilityHandler = require("../backend/FacilityHandler.js"); // Requiring the mocked module
+    FacilityHandler.getFacilities.mockResolvedValueOnce(mockFacilities); // Mocking facilities with data
+    FacilityHandler.editFacility.mockResolvedValueOnce({});
+    const PropertyHandler = require("../backend/PropertyHandler.js"); // Requiring the mocked module
+    const mockpropertyName = { propertyName: "Test Property" };
+    PropertyHandler.getPropertyData.mockResolvedValueOnce(mockpropertyName); // Mocking facilities with data
 
-  // Additional tests can be added here
+    await act(async () => {
+      render(
+        <MemoryRouter>
+          <PropertyFacilities />
+        </MemoryRouter>
+      );
+    });
+    // Check if the title is rendered
+    expect(screen.getByText("Test Property Facilities")).toBeInTheDocument();
+
+    // Check if the Add Facility button is rendered
+    expect(screen.getByText("Add Facility")).toBeInTheDocument();
+
+    // Asserting that the component renders with facilities
+    await waitFor(() => {
+      expect(screen.getByText("Gym")).toBeInTheDocument();
+    });
+    await waitFor(() => {
+      expect(screen.getByText("Pool")).toBeInTheDocument();
+    });
+
+    const deleteBtns = screen.getAllByText("Delete");
+    fireEvent.click(deleteBtns[0]);
+  });
 });
